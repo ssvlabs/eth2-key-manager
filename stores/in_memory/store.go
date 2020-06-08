2bb7b71cd34dbb01a8d8d63df7241029c762a92e
Alon Muroch <alonmuroch@gmail.com>
2020-06-08 18:57:03 +0300

    inmem store cleanup

--- stores/in_memory/store.go
+++ stores/in_memory/store.go
@@ -2,7 +2,6 @@ package in_memory
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/bloxapp/KeyVault/core"
 	uuid "github.com/google/uuid"
 	types "github.com/wealdtech/go-eth2-wallet-types/v2"
@@ -176,12 +175,7 @@ func (store *InMemStore) maybeEncrypt(input interface{}) ([]byte,error) {
 		return nil,err
 	}
 
-	canEncrypt, err := store.verifyCanEncrypt()
-	if err != nil {
-		return nil,err
-	}
-
-	if canEncrypt {
+	if store.verifyCanEncrypt() {
 		encrypted,err := store.encryptor.Encrypt(data,store.encryptionPassword)
 		if err != nil {
 			return nil,err
@@ -196,11 +190,7 @@ func (store *InMemStore) maybeEncrypt(input interface{}) ([]byte,error) {
 }
 
 func (store *InMemStore) maybeDecrypt(input []byte, ret interface{}) error {
-	canEncrypt, err := store.verifyCanEncrypt()
-	if err != nil {
-		return err
-	}
-	if canEncrypt {
+	if store.verifyCanEncrypt() {
 		// get encrypted data
 		var data map[string]interface{}
 		err := json.Unmarshal(input,&data)
@@ -215,142 +205,19 @@ func (store *InMemStore) maybeDecrypt(input []byte, ret interface{}) error {
 		}
 
 		// unmarshal to object
-		return json.Unmarshal(decrypted,ret)
+		return json.Unmarshal(decrypted,&ret)
 	} else {
 		// if not encrypted just unmarshal
 		return json.Unmarshal(input,&ret)
 	}
 }
 
-func (store *InMemStore) verifyCanEncrypt() (bool,error) {
+func (store *InMemStore) verifyCanEncrypt() bool {
 	if store.encryptor != nil {
 		if store.encryptionPassword == nil {
-			return false, fmt.Errorf("can't encrypt, missing password")
+			return false
 		}
-		return true,nil
+		return true
 	}
-	return false,nil
+	return false
 }
-
-//// Name provides the name of the store
-//func (store *InMemStore) Name() string {
-//	return "in-memory"
-//}
-//
-//// StoreWallet stores wallet data.  It will fail if it cannot store the data.
-//func (store *InMemStore) StoreWallet(walletID uuid.UUID, walletName string, data []byte) error {
-//	if val := store.memory[walletID.String()]; val != nil { // existing wallet
-//		store.memory[walletID.String()]["wallet"] = data
-//	} else {
-//		store.memory[walletID.String()] = map[string][]byte { // new wallet
-//			"wallet":data,
-//		}
-//		store.mapNameToId[walletName] = walletID
-//	}
-//	return nil
-//}
-//
-//// RetrieveWallet retrieves wallet data for all wallets.
-//func (store *InMemStore) RetrieveWallets() <-chan []byte {
-//	ch := make(chan []byte, 1024)
-//
-//	go func() {
-//		for _, w := range store.memory {
-//			ch <- w["wallet"]
-//		}
-//		close(ch)
-//	}()
-//
-//	return ch
-//}
-//
-//// RetrieveWallet retrieves wallet data for a wallet with a given name.
-//// It will fail if it cannot retrieve the data.
-//func (store *InMemStore) RetrieveWallet(walletName string) ([]byte, error) {
-//	w, err := store.walletByName(walletName)
-//	if err != nil {
-//		return nil, err
-//	}
-//	return w["wallet"],nil
-//}
-//
-//// RetrieveWalletByID retrieves wallet data for a wallet with a given ID.
-//// It will fail if it cannot retrieve the data.
-//func (store *InMemStore) RetrieveWalletByID(walletID uuid.UUID) ([]byte, error) {
-//	w, err := store.walletById(walletID)
-//	if err != nil {
-//		return nil,err
-//	}
-//	return w["wallet"],nil
-//}
-//
-//// StoreAccount stores account data.  It will fail if it cannot store the data.
-//func (store *InMemStore) StoreAccount(walletID uuid.UUID, accountID uuid.UUID, data []byte) error {
-//	wallet,error := store.walletById(walletID)
-//	if error != nil {
-//		return error
-//	}
-//
-//	wallet[accountID.String()] = data
-//	return nil
-//}
-//
-//// RetrieveAccounts retrieves account information for all accounts.
-//func (store *InMemStore) RetrieveAccounts(walletID uuid.UUID) <-chan []byte {
-//	ch := make(chan []byte, 1024)
-//
-//	go func() {
-//		if wallet, err := store.walletById(walletID); err == nil {
-//			for key, a := range wallet {
-//				if key != "wallet" {
-//					ch <- a
-//				}
-//			}
-//		}
-//
-//		close(ch)
-//	}()
-//
-//	return ch
-//}
-//
-//// RetrieveAccount retrieves account data for a wallet with a given ID.
-//// It will fail if it cannot retrieve the data.
-//func (store *InMemStore) RetrieveAccount(walletID uuid.UUID, accountID uuid.UUID) ([]byte, error) {
-//	wallet,error := store.walletById(walletID)
-//	if error != nil {
-//		return nil,error
-//	}
-//
-//	if val := wallet[accountID.String()]; val != nil {
-//		return val,nil
-//	}
-//
-//	return nil, fmt.Errorf("account id %s in wallet id %s, not found",accountID.String(), walletID.String())
-//}
-//
-//// StoreAccountsIndex stores the index of accounts for a given wallet.
-//func (store *InMemStore) StoreAccountsIndex(walletID uuid.UUID, data []byte) error {
-//	store.accountIndx[walletID.String()] = data
-//	return nil
-//}
-//
-//// RetrieveAccountsIndex retrieves the index of accounts for a given wallet.
-//func (store *InMemStore) RetrieveAccountsIndex(walletID uuid.UUID) ([]byte, error) {
-//	return store.accountIndx[walletID.String()], nil
-//}
-//
-//func (store *InMemStore) walletByName(walletName string) (map[string][]byte,error) {
-//	if walletId, ok := store.mapNameToId[walletName]; ok {
-//		return store.walletById(walletId)
-//	}
-//	return nil,fmt.Errorf("wallet not found") // important as github.com/wealdtech/go-eth2-wallet-hd looks for this error
-//}
-//
-//func (store *InMemStore) walletById(walletID uuid.UUID) (map[string][]byte,error) {
-//	w := store.memory[walletID.String()]
-//	if w == nil {
-//		return nil, fmt.Errorf("wallet not found") // important as github.com/wealdtech/go-eth2-wallet-hd looks for this error
-//	}
-//	return w,nil
-//}
