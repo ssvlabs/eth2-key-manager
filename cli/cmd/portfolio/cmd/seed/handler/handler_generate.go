ef2fa569c3b5392fa7ae4471042ec1c01baa04e5
Oleg Shmuelov <oleg@blox.io>
2020-07-20 19:22:57 +0300

    generate seed handler

--- cli/cmd/portfolio/cmd/seed/handler/handler_generate.go
+++ cli/cmd/portfolio/cmd/seed/handler/handler_generate.go
@@ -12,18 +12,47 @@ import (
 
 // Seed generates a new portfolio seed and prints it.
 func (h *Seed) Generate(cmd *cobra.Command, args []string) error {
+
+	var seed []byte
+	var mnemonic string
+
 	// Get mnemonic flag.
-	mnemonic, err := flag.GetMnemonicFlagValue(cmd)
+	mnemonicFlagValue, err := flag.GetMnemonicFlagValue(cmd)
 	if err != nil {
 		return errors.Wrap(err, "failed to retrieve the mnemonic flag value")
 	}
 
-	h.printer.JSON(mnemonic)
-
-	seed, err := KeyVault.GenerateNewSeed()
+	// Get seed flag.
+	seedFlagValue, err := flag.GetSeedFlagValue(cmd)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "failed to retrieve the seed flag value")
 	}
-	h.printer.Text(hex.EncodeToString(seed))
+
+	// Generate seed if seed flag is not provided.
+	if len(seedFlagValue) == 0 {
+		seed, err = KeyVault.GenerateNewSeed()
+		if err != nil {
+			return errors.Wrap(err, "failed to generate new seed")
+		}
+	} else if mnemonicFlagValue {
+		seed, err = hex.DecodeString(seedFlagValue)
+		if err != nil {
+			return errors.Wrap(err, "failed to hex decode the seed flag value")
+		}
+	}
+
+	// Generate mnemonic
+	if mnemonicFlagValue {
+		mnemonic, err = KeyVault.SeedToMnemonic(seed)
+		if err != nil {
+			return errors.Wrap(err, "failed to generate mnemonic from seed")
+		}
+		h.printer.Text(mnemonic)
+	} else if len(seedFlagValue) > 0 {
+		h.printer.Text(seedFlagValue)
+	} else {
+		h.printer.Text(hex.EncodeToString(seed))
+	}
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,17 +3,20 @@ module github.com/bloxapp/KeyVault
 go 1.13
 
 require (
-	github.com/ethereum/go-ethereum v1.9.15
 	github.com/google/uuid v1.1.1
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/vault/sdk v0.1.13
 	github.com/pkg/errors v0.9.1
 	github.com/prysmaticlabs/ethereumapis v0.0.0-20200608211251-7dafd77461b5
 	github.com/prysmaticlabs/go-ssz v0.0.0-20200101200214-e24db4d9e963
 	github.com/spf13/cobra v1.0.0
 	github.com/stretchr/testify v1.6.1
+	github.com/tyler-smith/go-bip39 v1.0.1-0.20181017060643-dbb3b84ba2ef
 	github.com/wealdtech/eth2-signer-api v1.3.0
 	github.com/wealdtech/go-eth2-types/v2 v2.4.0
 	github.com/wealdtech/go-eth2-util v1.2.0
 	github.com/wealdtech/go-eth2-wallet-encryptor-keystorev4 v1.0.0
 	github.com/wealdtech/go-eth2-wallet-types/v2 v2.0.2
+	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
+	golang.org/x/text v0.3.2 // indirect
 )
