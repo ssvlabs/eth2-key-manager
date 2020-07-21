a4d86114e978dfb9e8e7d9b8acc529161b7fb078
Oleg Shmuelov <oleg@blox.io>
2020-07-21 12:45:02 +0300

    test seed generate

--- cli/cmd/portfolio/cmd/seed/generate_test.go
+++ cli/cmd/portfolio/cmd/seed/generate_test.go
@@ -11,20 +11,64 @@ import (
 )
 
 func TestSeedGenerate(t *testing.T) {
-	var output bytes.Buffer
-	cmd.ResultPrinter = printer.New(&output)
-
-	// Execute
-	cmd.RootCmd.SetArgs([]string{
-		"portfolio",
-		"seed",
-		"generate",
+	t.Run("Successfully Generate Seed", func(t *testing.T) {
+		var output bytes.Buffer
+		cmd.ResultPrinter = printer.New(&output)
+		cmd.RootCmd.SetArgs([]string{
+			"portfolio",
+			"seed",
+			"generate",
+		})
+		err := cmd.RootCmd.Execute()
+		require.NoError(t, err)
+		actualOutput := output.String()
+		require.NotNil(t, actualOutput)
 	})
-	err := cmd.RootCmd.Execute()
 
-	// Result
-	require.NoError(t, err)
+	t.Run("Successfully Generate Mnemonic", func(t *testing.T) {
+		var output bytes.Buffer
+		cmd.ResultPrinter = printer.New(&output)
+		cmd.RootCmd.SetArgs([]string{
+			"portfolio",
+			"seed",
+			"generate",
+			"--mnemonic",
+		})
+		err := cmd.RootCmd.Execute()
+		require.NoError(t, err)
+		actualOutput := output.String()
+		require.NotNil(t, actualOutput)
+	})
 
-	actualOutput := output.String()
-	require.Equal(t, "TODO: Implement seed generating\n", actualOutput)
+	t.Run("Successfully Generate Mnemonic from seed", func(t *testing.T) {
+		var output bytes.Buffer
+		cmd.ResultPrinter = printer.New(&output)
+		cmd.RootCmd.SetArgs([]string{
+			"portfolio",
+			"seed",
+			"generate",
+			"--mnemonic",
+			"--seed=a42b2d973095bb518e45ae5b372dbff9a3aec572ff74b1c8c54749d34b4479eb",
+		})
+		err := cmd.RootCmd.Execute()
+		require.NoError(t, err)
+		actualOutput := output.String()
+		require.Equal(t, "picnic floor grape gentle forum potato decorate remind forest ride husband viable depend glance slogan update range economy fade neck cruise peasant tray illegal\n", actualOutput)
+	})
+
+	t.Run("Don't generate seed if seed flag is provided", func(t *testing.T) {
+		var output bytes.Buffer
+		cmd.ResultPrinter = printer.New(&output)
+		cmd.RootCmd.SetArgs([]string{
+			"portfolio",
+			"seed",
+			"generate",
+			"--mnemonic=false",
+			"--seed=a42b2d973095bb518e45ae5b372dbff9a3aec572ff74b1c8c54749d34b4479eb",
+		})
+		err := cmd.RootCmd.Execute()
+		require.NoError(t, err)
+		actualOutput := output.String()
+		require.Equal(t, "a42b2d973095bb518e45ae5b372dbff9a3aec572ff74b1c8c54749d34b4479eb\n", actualOutput)
+	})
 }
