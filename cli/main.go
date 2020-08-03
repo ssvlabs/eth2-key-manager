8193d912108c5cb30a673db8cd9772f52a3bf15c
Oleg Shmuelov <oleg@blox.io>
2020-08-03 17:55:07 +0300

    add mnemonic command

--- cli/main.go
+++ cli/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/bloxapp/KeyVault/cli/cmd"
+	_ "github.com/bloxapp/KeyVault/cli/cmd/mnemonic"
 	_ "github.com/bloxapp/KeyVault/cli/cmd/seed"
 	_ "github.com/bloxapp/KeyVault/cli/cmd/wallet"
 	_ "github.com/bloxapp/KeyVault/cli/cmd/wallet/cmd/account"
