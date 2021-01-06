f7d66255ae269ba80aee4e4c5d5b6ac23c7597e6
Roman Behma <begmaroman@gmail.com>
2021-01-06 11:22:24 +0200

    Implementation

--- signer/far_future_protection.go
+++ signer/far_future_protection.go
@@ -8,8 +8,8 @@ import (
 	"github.com/bloxapp/eth2-key-manager/core"
 )
 
-// FarFutureMaxValidEpoch is the max epoch of fat future signing
-var FarFutureMaxValidEpoch = int64(time.Hour.Hours()) * 6
+// FarFutureMaxValidEpoch is the max epoch of far future signing
+var FarFutureMaxValidEpoch = int64(time.Minute.Seconds() * 20)
 
 // IsValidFarFutureEpoch prevents far into the future signing request, verify a slot is within the current epoch
 // https://github.com/ethereum/eth2.0-specs/blob/dev/specs/phase0/validator.md#protection-best-practices
