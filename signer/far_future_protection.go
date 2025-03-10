87548a6e18ae07c54f47aa0783bbad400d69e73f
y0sher <lyosher@gmail.com>
2025-03-10 13:40:12 +0200

    align iface usage

--- signer/far_future_protection.go
+++ signer/far_future_protection.go
@@ -7,17 +7,17 @@ import (
 )
 
 // FarFutureMaxValidEpoch is the max epoch of far future signing
-var FarFutureMaxValidEpoch = int64(time.Minute.Seconds() * 20)
+var FarFutureMaxValidEpoch = time.Minute * 20
 
 // IsValidFarFutureEpoch prevents far into the future signing request, verify a slot is within the current epoch
 // https://github.com/ethereum/eth2.0-specs/blob/dev/specs/phase0/validator.md#protection-best-practices
 func IsValidFarFutureEpoch(network network, epoch phase0.Epoch) bool {
-	maxValidEpoch := network.EstimatedEpochAtSlot(network.EstimatedSlotAtTime(time.Now().Unix() + FarFutureMaxValidEpoch))
+	maxValidEpoch := network.EstimatedEpochAtSlot(network.EstimatedSlotAtTime(time.Now().Add(FarFutureMaxValidEpoch)))
 	return epoch <= maxValidEpoch
 }
 
 // IsValidFarFutureSlot returns true if the given slot is valid
 func IsValidFarFutureSlot(network network, slot phase0.Slot) bool {
-	maxValidSlot := network.EstimatedSlotAtTime(time.Now().Unix() + FarFutureMaxValidEpoch)
+	maxValidSlot := network.EstimatedSlotAtTime(time.Now().Add(FarFutureMaxValidEpoch))
 	return slot <= maxValidSlot
 }
