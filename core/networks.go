79d57698b37acb8d653cf80644e82cd0f085dd0f
Matus Kysel <matus@ssvlabs.io>
2025-03-17 10:54:39 +0100

    feat: add support of hoodi

--- core/networks.go
+++ core/networks.go
@@ -20,6 +20,8 @@ func NetworkFromString(n string) Network {
 		return PraterNetwork
 	case string(HoleskyNetwork):
 		return HoleskyNetwork
+	case string(HoodiNetwork):
+		return HoodiNetwork
 	case string(MainNetwork):
 		return MainNetwork
 	default:
@@ -36,6 +38,8 @@ func (n Network) GenesisForkVersion() phase0.Version {
 		return phase0.Version{0x00, 0x00, 0x10, 0x20}
 	case HoleskyNetwork:
 		return phase0.Version{0x01, 0x01, 0x70, 0x00}
+	case HoodiNetwork:
+		return phase0.Version{0x10, 0x00, 0x09, 0x10}
 	case MainNetwork:
 		return phase0.Version{0, 0, 0, 0}
 	default:
@@ -53,6 +57,9 @@ func (n Network) GenesisValidatorsRoot() phase0.Root {
 		copy(genValidatorsRoot[:], rootBytes)
 	case HoleskyNetwork:
 		rootBytes, _ := hex.DecodeString("9143aa7c615a7f7115e2b6aac319c03529df8242ae705fba9df39b79c59fa8b1")
+		copy(genValidatorsRoot[:], rootBytes)
+	case HoodiNetwork:
+		rootBytes, _ := hex.DecodeString("212f13fc4df078b6cb7db228f1c8307566dcecf900867401a92023d7ba99cb5f")
 		copy(genValidatorsRoot[:], rootBytes)
 	case MainNetwork:
 		rootBytes, _ := hex.DecodeString("4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95")
@@ -72,6 +79,8 @@ func (n Network) DepositContractAddress() string {
 		return "0xff50ed3d0ec03ac01d4c79aad74928bff48a7b2b"
 	case HoleskyNetwork:
 		return "0x4242424242424242424242424242424242424242"
+	case HoodiNetwork:
+		return "0x00000000219ab540356cBB839Cbe05303d7705Fa"
 	case MainNetwork:
 		return "0x00000000219ab540356cBB839Cbe05303d7705Fa"
 	default:
@@ -94,6 +103,8 @@ func (n Network) MinGenesisTime() uint64 {
 		return 1616508000
 	case HoleskyNetwork:
 		return 1695902400
+	case HoodiNetwork:
+		return 1742212800 + 600
 	case MainNetwork:
 		return 1606824023
 	default:
@@ -147,6 +158,9 @@ const (
 
 	// HoleskyNetwork represents the Holesky test network.
 	HoleskyNetwork Network = "holesky"
+
+	// HoodiNetwork represents the Hoodi test network.
+	HoodiNetwork Network = "hoodi"
 
 	// MainNetwork represents the main network.
 	MainNetwork Network = "mainnet"
